227967016ca8919b8736a4ebcd523f1d4ec2314f
TiDz <67941515+tidzvl@users.noreply.github.com>
2024-03-06 06:30:02 -0500

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -17,17 +17,8 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 
-
+//func of users/ in firebcase - booking only
 (function (){
-	function writeUser(userId, name, email, yo){
-		set(ref(database, 'users/' + userId), {
-		    username: name,
-		    email: email,
-		    year_old : yo
-		  });
-		  console.log("Writed!");
-	}
-
 	function readUser(userId){
 		const dbRef = ref(database);
 		get(child(dbRef, `users/${userId}`)).then((snapshot) => {
@@ -41,6 +32,59 @@ const database = getDatabase(app);
 		});
 	}
 
+	function getLastID() {
+	  return new Promise((resolve, reject) => {
+	    const dbRef = ref(database);
+	    get(child(dbRef, `booking/`)).then((snapshot) => {
+	      if (snapshot.exists()) {
+	        var output = snapshot.val().length;
+	        resolve(output);
+	      } else {
+	        resolve("0");
+	      }
+	    }).catch((error) => {
+	      console.error(error);
+	      reject(error);
+	    });
+	  });
+	}
+
+
+	function validateForm() {
+	  var form = document.getElementById('bookingform');
+	  for (var i = 0; i < form.elements.length; i++) {
+	    if (form.elements[i].value === '' && form.elements[i].hasAttribute('required')) {
+	      return false;
+	    }
+	  }
+	  return true;
+	}
+
+	async function insertNewBooking(name, yo, sex, contact, address = "", bhyt = "No", bhytnum = "0", des = "", khoa = "Kham thuong"){
+		let id = await getLastID();
+		set(ref(database, 'booking/' + id), {
+	    fullname: name,
+	    year_old: yo,
+	    sex: sex,
+	    contact: contact,
+	    address: address,
+	    bhyt: bhyt,
+	    bhytnum: bhytnum,
+	    des: des,
+	    khoa: khoa
+	  });
+	  console.log("User id "+id+" have been create! Run console: readUser("+id+") to view this!");
+	}
+
 	// document.getElementById('book').addEventListener('click', () => {writeUser("2","TiDz","tidz@ti.com","22");}, true);
-	document.getElementById('book').addEventListener('click', () => {readUser("2");}, true);
+	// document.getElementById('book').addEventListener('click', () => {readUser("3");}, true);
+	document.getElementById('submit').addEventListener('click', () => {
+		if(validateForm()){
+			var booking_in4 = document.getElementById('bookingform');
+			var booking_data = new FormData(booking_in4);
+			var arr = Array.from(booking_data.entries());
+			var khoa = document.getElementById('khoa').value;
+			insertNewBooking(arr[0][1],arr[1][1],arr[2][1],arr[3][1],arr[4][1],arr[5][1],arr[6][1],arr[7][1],khoa);
+		}
+	}, true);
 })();
